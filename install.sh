7c7c9fc576790ebbd840a9c4606cdb3a2230a6cd
Kittelchen <kittelchen@github.com>
2026-03-26 00:04:35 +0100

    update script, so now there is a noctalia command to kill and restart the process properly, also included in the install script

--- install.sh
+++ install.sh
@@ -6,24 +6,28 @@ RICE_DIR="$(cd "$(dirname "$0")" && pwd)"
 HYPR_DIR="$HOME/.config/hypr"
 KITTY_DIR="$HOME/.config/kitty"
 FISH_DIR="$HOME/.config/fish"
+BIN_DIR="$HOME/.local/bin"
 
 COPY_HYPR=false
 COPY_KITTY=false
 COPY_FISH=false
+COPY_BIN=false
 
 for arg in "$@"; do
   case $arg in
     --hypr)  COPY_HYPR=true ;;
     --kitty) COPY_KITTY=true ;;
     --fish)  COPY_FISH=true ;;
+    --bin)   COPY_BIN=true ;;
   esac
 done
 
 # if no flags given, copy everything
-if ! $COPY_HYPR && ! $COPY_KITTY && ! $COPY_FISH; then
+if ! $COPY_HYPR && ! $COPY_KITTY && ! $COPY_FISH && ! $COPY_BIN; then
   COPY_HYPR=true
   COPY_KITTY=true
   COPY_FISH=true
+  COPY_BIN=true
 fi
 
 if $COPY_HYPR; then
@@ -45,4 +49,11 @@ if $COPY_FISH; then
   mkdir -p "$FISH_DIR"
   cp "$RICE_DIR/dotfiles/fish/config.fish" "$FISH_DIR/"
   echo "✔ fish done!"
+fi
+
+if $COPY_BIN; then
+  echo "Copying bin files..."
+  mkdir -p "$BIN_DIR"
+  cp "$RICE_DIR/dotfiles/bin/"* "$BIN_DIR/"
+  echo "✔ bin done!"
 fi
